3b794a1e439624d097a452da32d6e4129b8c0449
Davor Cuvalo <adriatic111@yahoo.com>
2017-10-17 16:13:33 +0200

    New changes

--- cms-new/admin/includes/add_post.php
+++ cms-new/admin/includes/add_post.php
@@ -2,7 +2,7 @@
 if (isset($_POST['create_post'])) {
     
     $post_title = $_POST['title'];
-    $post_category_id = $_POST['post_category_id'];
+    $post_category_id = $_POST['post_category'];
     $post_author = $_POST['author'];
     $post_status = $_POST['post_status'];
    
@@ -34,11 +34,38 @@ confirm($create_post_query, $post_category_id); // funkcija za potvrdu queryja, 
     <label for="title">Post Title</label>
     <input type="text" class="form-control" name="title">    
 </div>
-
+<!--
 <div class="form-group">
     <label for="post_category">Post Category Id (number)</label>
     <input type="text" class="form-control" name="post_category_id">    
 </div>
+-->
+
+<!-- pulldown menu -->
+<div class="form-group">
+    <select name="post_category" id="">
+<?php
+$query = "SELECT * FROM categories"; 
+    $select_categories = mysqli_query($connection, $query);
+
+    confirmQuery($select_categories);  // za potvrdu queryja
+
+while ($row = mysqli_fetch_assoc($select_categories)) {
+    $cat_id = $row['cat_id'];
+    $cat_title = $row['cat_title']; 
+
+    echo "<option value='$cat_id'>$cat_title</option>";   
+      
+}
+?>
+    </select>
+</div>
+
+
+
+
+
+
 
 <div class="form-group">
     <label for="author">Post Author</label>
--- cms-new/admin/includes/edit_post.php
+++ cms-new/admin/includes/edit_post.php
@@ -2,7 +2,7 @@
 if (isset($_GET['p_id'])) {
     $the_post_id = $_GET['p_id'];
 }
-
+// odaber post za editiranje na osnovu p_id poslanom u adresi sa GET
 $query = "SELECT * FROM posts WHERE post_id = '$the_post_id' "; 
 $select_posts_by_id = mysqli_query($connection, $query);
 
@@ -19,6 +19,44 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     $post_date = $row['post_date'];
 }
 
+// ako je kliknuo Update updataj post
+if (isset($_POST['update_post'])) {
+    
+    $post_title = $_POST['post_title'];
+    $post_author = $_POST['author'];
+    $post_category_id = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_content = $_POST['post_content'];
+    $post_tags = $_POST['post_tags'];
+
+move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    if (empty($post_image)) {  // ako nisam odabrao novi image, updataj stari iz baze
+        $query = "SELECT * FROM posts";
+        $select_image =mysqli_query($connection, $query);
+
+        while ($row = mysqli_fetch_array($select_image)) {
+            $post_image = $row['post_image'];
+        }
+    }
+
+    $query = "UPDATE posts SET ";
+    $query .= "post_title = '$post_title', ";
+    $query .= "post_category_id = '$post_category_id', ";
+    $query .= "post_date = now(), ";
+    $query .= "post_author = '$post_author', ";
+    $query .= "post_status = '$post_status', ";
+    $query .= "post_tags = '$post_tags', ";
+    $query .= "post_content = '$post_content', ";
+    $query .= "post_image = '$post_image' ";
+    $query .= "WHERE post_id = '$the_post_id' ";
+
+    $update_post = mysqli_query($connection, $query);
+    confirmQuery($update_post);
+
+};
 ?>
 
 
@@ -26,11 +64,12 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
 <div class="form-group">
     <label for="title">Post Title</label>
-    <input type="text" value="<?php echo $post_title; ?>" class="form-control" name="title">    
+    <input type="text" value="<?php echo $post_title; ?>" class="form-control" name="post_title">    
 </div>
 
+<!-- pulldown menu -->
 <div class="form-group">
-    <select name="" id="">
+    <select name="post_category" id="">
 <?php
 $query = "SELECT * FROM categories"; 
     $select_categories = mysqli_query($connection, $query);
@@ -41,7 +80,7 @@ while ($row = mysqli_fetch_assoc($select_categories)) {
     $cat_id = $row['cat_id'];
     $cat_title = $row['cat_title']; 
 
-    echo "<option value=''>$cat_title</option>";   
+    echo "<option value='$cat_id' >$cat_title</option>";   
       
 }
 ?>
@@ -61,7 +100,7 @@ while ($row = mysqli_fetch_assoc($select_categories)) {
 <div class="form-group">
     <label for="image">Post Image</label>
     <img src="../images/<?php echo $post_image; ?>" class="img-responsive" style="max-height: 80px">
-    <input type="file" value="<?php echo $post_image; ?>" class="form-control" name="image">    
+    <input type="file" value="" class="form-control" name="image">   
 </div>
 
 <div class="form-group">
@@ -76,7 +115,7 @@ while ($row = mysqli_fetch_assoc($select_categories)) {
 </div>
 
 <div class="form-group">
-<input type="submit" class="btn btn-primary" name="create_post" value="Publish Post">
+<input type="submit" class="btn btn-primary" name="update_post" value="Publish Post">
 </div>
 
 </form>
